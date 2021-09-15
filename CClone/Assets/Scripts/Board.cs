d6c5b62186f97c0d1d13f4e82f0e8e693bc263d6
furkankupcu <51542849+furkankupcu@users.noreply.github.com>
2021-09-15 23:07:38 +0300

    Part 9

--- CClone/Assets/Scripts/Board.cs
+++ CClone/Assets/Scripts/Board.cs
@@ -7,7 +7,7 @@ public class Board : MonoBehaviour
     [SerializeField]public int width; //grid alaný
     [SerializeField]public int height;
 
-    public GameObject[] dots;//oluþturalacak assetler 
+    public GameObject[] dots;//olusturalacak assetler 
 
     public GameObject tilePrefab;
 
@@ -36,7 +36,16 @@ public class Board : MonoBehaviour
                 backgroundTile.name = "(" + i + "," + j + ")";
 
                 //kullanýlacak assetler,þekerler
+
                 int dotToUse = Random.Range(0, dots.Length);
+
+                int maxIterations = 0; 
+                while (MachesAt(i, j, dots[dotToUse]) && maxIterations < 100){
+                    dotToUse = Random.Range(0, dots.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
                 GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
                 dot.transform.parent = this.transform;
                 dot.name = "(" + i + "," + j + ")";
@@ -45,6 +54,43 @@ public class Board : MonoBehaviour
         }
 
     }
+
+    private bool MachesAt(int column,int row, GameObject piece)
+    {
+        if (column > 1 && row >1)
+        {
+            if (allDots[column-1,row].tag == piece.tag && allDots[column-2,row].tag == piece.tag)
+            {
+                return true;
+            }
+            if (allDots[column, row-1].tag == piece.tag && allDots[column, row-2].tag == piece.tag)
+            {
+                return true;
+            }
+
+        }else if(column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if(allDots[column,row-1].tag == piece.tag && allDots[column,row-2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allDots[column-1, row].tag == piece.tag && allDots[column-2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
 
 
 
--- CClone/Assets/Scripts/Dot.cs
+++ CClone/Assets/Scripts/Dot.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
+    [Header("Board Variables")]
     public int column;
     public int row;
+    public int previousColumn;
+    public int previousRow;
     public int targetX;
     public int targetY;
+    public bool isMatched = false;
     private Board board;
     private GameObject otherDot;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
     private Vector2 tempPosition;
     public float swipeAngle = 0;
+    public float swipeResist = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +28,21 @@ public class Dot : MonoBehaviour
         targetY = (int)transform.position.y;
         row = targetY;
         column = targetX;
+        previousRow = row;
+        previousColumn = column;
     }
 
     // Update is called once per frame
     void Update()
     {
+        FindMatches();
+
+        if (isMatched)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f,.2f);
+        }
+
         targetX = column;
         targetY = row;
         if(Mathf.Abs(targetX - transform.position.x) > .1)
@@ -58,6 +73,26 @@ public class Dot : MonoBehaviour
         }
     }
 
+    public IEnumerator CheckMoveCo() 
+        //Coroutine kodun çalýþmasýný geciktirme,iþlemin bitmesini bekleme vb.
+    {
+        yield return new WaitForSeconds(.5f);
+        if(otherDot != null)
+        {
+            if (!isMatched && !otherDot.GetComponent<Dot>().isMatched)
+            {
+                otherDot.GetComponent<Dot>().row = row;
+                otherDot.GetComponent<Dot>().column = column;
+                row = previousRow;
+                column = previousColumn;
+            }
+            otherDot = null;
+        }
+    }
+
+
+
+
     private void OnMouseDown()
     {
         firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -72,22 +107,24 @@ public class Dot : MonoBehaviour
 
     void CalculateAngle()
     {
-
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        //Debug.Log(swipeAngle);
-        MovePieces();
+        if (Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+            //Debug.Log(swipeAngle);
+            MovePieces();
+        }
     }
 
     void MovePieces()
     {
-        if(swipeAngle > -45 && swipeAngle <= 45 && column < board.width)
+        if(swipeAngle > -45 && swipeAngle <= 45 && column < board.width -1 )
         {
             //Right Swipe
             otherDot = board.allDots[column + 1, row];
             otherDot.GetComponent<Dot>().column -= 1;
             column += 1;
         }
-        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height-1)
         {
             //Up Swipe
             otherDot = board.allDots[column, row +1];
@@ -108,7 +145,48 @@ public class Dot : MonoBehaviour
             otherDot.GetComponent<Dot>().row += 1;
             row -= 1;
         }
+        StartCoroutine(CheckMoveCo());
     }
+
+    void FindMatches()
+    {      //yatay eslesme
+        if (column > 0 && column < board.width - 1)
+        {  //sag ve sol noktalarý al
+            GameObject leftDot1 = board.allDots[column-1,row];
+            GameObject rightDot1 = board.allDots[column + 1, row];
+            if (leftDot1 != null && rightDot1 != null)
+            {
+                //sað ve sol noktalarý kontrol et
+                if (leftDot1.tag == this.gameObject.tag && rightDot1.tag == this.gameObject.tag)
+                {
+                    //eslestiyse isMatched true
+                    leftDot1.GetComponent<Dot>().isMatched = true;
+                    rightDot1.GetComponent<Dot>().isMatched = true;
+                    isMatched = true; //bu nokta için true
+                }
+            }
+        }
+        //dikey eslesme
+        if (row > 0 && row < board.height - 1)
+        {  //sag ve sol noktalarý al
+            GameObject upDot1 = board.allDots[column, row+1];
+            GameObject downDot1 = board.allDots[column, row-1];
+
+            if (upDot1 != null && downDot1 != null)
+            {
+                //sað ve sol noktalarý kontrol et
+                if (upDot1.tag == this.gameObject.tag && downDot1.tag == this.gameObject.tag)
+                {
+                    //eslestiyse isMatched true
+                    upDot1.GetComponent<Dot>().isMatched = true;
+                    downDot1.GetComponent<Dot>().isMatched = true;
+                    isMatched = true; //bu nokta için true
+                }
+            }
+        }
+
+    }
+    
 
 
 }
