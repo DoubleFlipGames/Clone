d86027abe28677cf9e21f08658fd83aff80e0aed
furkankupcu <51542849+furkankupcu@users.noreply.github.com>
2021-10-07 22:29:32 +0300

    ++++++

--- CClone/Assets/Scripts/FindMatches.cs
+++ CClone/Assets/Scripts/FindMatches.cs
@@ -89,48 +89,58 @@ public class FindMatches : MonoBehaviour
             for (int j = 0; j < board.height; j++)
             {
                 GameObject currentDot = board.allDots[i, j];
-                Dot currentDotDot = currentDot.GetComponent<Dot>();
+                
 
                 if (currentDot != null)
                 {
+                    Dot currentDotDot = currentDot.GetComponent<Dot>();
                     if (i>0 && i < board.width -1)
                     {
                         GameObject leftDot = board.allDots[i - 1, j];
-                        Dot leftDotDot = leftDot.GetComponent<Dot>();
+                        
                         GameObject rightDot = board.allDots[i + 1, j];
-                        Dot rightDotDot = rightDot.GetComponent<Dot>();
-                        if (leftDot != null && rightDot != null)
+                        if (leftDot != null && rightDot !=null)
                         {
-                            if (leftDot.tag == currentDot.tag && rightDot.tag == currentDot.tag)
+                            
+                            Dot leftDotDot = leftDot.GetComponent<Dot>();
+                            Dot rightDotDot = rightDot.GetComponent<Dot>();
+
+                            if (leftDot != null && rightDot != null)
                             {
-                                currentMatches.Union(IsRowBomb(leftDotDot, currentDotDot, rightDotDot));
+                                if (leftDot.tag == currentDot.tag && rightDot.tag == currentDot.tag)
+                                {
+                                    currentMatches.Union(IsRowBomb(leftDotDot, currentDotDot, rightDotDot));
 
-                                currentMatches.Union(IsColumnBomb(leftDotDot, currentDotDot, rightDotDot));
+                                    currentMatches.Union(IsColumnBomb(leftDotDot, currentDotDot, rightDotDot));
 
-                                GetNearbyPieces(leftDot,currentDot,rightDot);
+                                    GetNearbyPieces(leftDot, currentDot, rightDot);
 
-                               
-                                
+                                }
                             }
                         }
                     }
 
                     if (j > 0 && j < board.height - 1)
                     {
-                        GameObject upDot = board.allDots[i, j+1];
-                        Dot upDotDot = upDot.GetComponent<Dot>();
-                        GameObject downDot = board.allDots[i , j-1];
-                        Dot downDotDot = downDot.GetComponent<Dot>();
+                        GameObject upDot = board.allDots[i, j + 1];
+                        GameObject downDot = board.allDots[i, j - 1];
                         if (upDot != null && downDot != null)
                         {
-                            if (upDot.tag == currentDot.tag && downDot.tag == currentDot.tag)
+
+
+                            Dot upDotDot = upDot.GetComponent<Dot>();
+                            Dot downDotDot = downDot.GetComponent<Dot>();
+                            if (upDot != null && downDot != null)
                             {
-                                currentMatches.Union(IsColumnBomb(upDotDot, currentDotDot, downDotDot));
+                                if (upDot.tag == currentDot.tag && downDot.tag == currentDot.tag)
+                                {
+                                    currentMatches.Union(IsColumnBomb(upDotDot, currentDotDot, downDotDot));
 
-                                currentMatches.Union(IsRowBomb(upDotDot,currentDotDot,downDotDot));
+                                    currentMatches.Union(IsRowBomb(upDotDot, currentDotDot, downDotDot));
 
-                                GetNearbyPieces(upDot, currentDot, downDot);
-                                                             
+                                    GetNearbyPieces(upDot, currentDot, downDot);
+
+                                }
                             }
                         }
                     }
